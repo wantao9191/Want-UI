d81d3ff9b0d20619905869776c0fa654d607f38d
wantao <13813872403>
2022-08-10 12:24:26 +0800

    'noticebar组件完成'

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import t from './App.module.scss'
 import { Toast } from '@/libs'
 export const App = defineComponent({
@@ -7,10 +7,40 @@ export const App = defineComponent({
             const toastAction = Toast({ text: '一个toast！' })
             toastAction.show()
         }
+        const visible = ref(true)
+        const close = () => {
+            visible.value = false
+        }
+        const slots = {
+            right: () => {
+                return <><want-button fill='none' size='mini' type='primary'>查看详情</want-button><want-button fill='none' size='mini'>关闭</want-button></>
+            }
+        }
+        const slots2 = {
+            right:()=>{
+                return <want-icon name='double-right'></want-icon>
+            }
+        }
         return () => (
             <div class={t.app}>
-                <want-noticebar></want-noticebar>
+                <div>
+                    <want-noticebar content='default' type='default'></want-noticebar>
+                    <want-noticebar content='info' type='info'></want-noticebar>
+                    <want-noticebar content='alert' type='alert'></want-noticebar>
+                    <want-noticebar content='error' type='error'></want-noticebar>
+                </div>
+                <div>
+                    <want-noticebar content='info' type='info' closeable v-show={visible.value} onClose={() => close()}></want-noticebar>
+                </div>
+                <div>
+                    <want-noticebar content='info' type='alert' icon='download'></want-noticebar>
+                </div>
+                <div>
+                <want-noticebar content='info' icon='download' type='error' v-slots={slots2}></want-noticebar>
+                    <want-noticebar content='右侧内容通过slots替换' icon='download' v-slots={slots}></want-noticebar>
+                </div>
             </div>
+
         )
     }
 })
--- src/libs/Noticebar/index.tsx
+++ src/libs/Noticebar/index.tsx
@@ -1,11 +1,22 @@
 import { defineComponent } from 'vue'
 import t from './index.module.scss'
 export const Noticebar = defineComponent({
-    setup(props, context) {
-        const slots = {
-            default:()=>(<span>123</span>)
+    props: {
+        content: String,
+        closeable: Boolean,
+        type: { type: String, default: 'default' },
+        icon: { type: String, default: 'sound' },
+        
+    },
+    setup(props, { slots, emit }) {
+        const close = () => {
+            emit('close')
         }
-        return () => (<div class={t['want-noticebar']} v-slots={slots}>
+        return () => (<div class={[t['want-noticebar'], t[`want-noticebar-${props.type}`]]}>
+            <span class={t['want-noticebar-left']}><want-icon name={props.icon} class={t['prefix']}></want-icon>{slots.default?.() ?? props.content}</span>
+            <span class={t['want-noticebar-right']}>
+                {slots.right?.() ?? <want-icon name='close' class={t['notice-close']} v-show={props.closeable} onClick={() => close()} />}
+            </span>
         </div>)
     }
 })
