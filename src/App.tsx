df871df7aa2b7a2ca2349748f4c161947ae4017c
467112199@qq.com <467112199@qq.com>
2022-08-11 22:58:46 +0800

    tabs超长滚动

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,17 @@ import t from './App.module.scss'
 import { dialog } from '@/libs'
 export const App = defineComponent({
     setup(props, context) {
-        const showBasic = () => {
-            dialog.confirm({ content: '基本用法 Confirm Dialog', confirmButtonText: '我知道了', cancelButtonType: 'danger', }).then(() => {
-                console.log('then')
-            }).catch(() => {
-                console.log('catch')
-            })
-        }
-        const showAlert = () => {
-            dialog.alert({ content: '基本用法 Alert Dialog' }).then(() => {
-                console.log('then')
-            })
-        }
-        const showAfter = () => {
-            dialog.confirm({
-                content: '异步关闭', beforeClose: (action: string, instance: any, done: any) => {
-                    console.log(action)
-                    if (action === 'confirm') {
-                        instance.confirmButtonLoading = true
-                        setTimeout(() => {
-                            instance.confirmButtonLoading = false
-                            done()
-                        }, 1000);
-                    } else {
-                        done()
-                    }
-
-                }
-            })
-        }
+        const activeName = ref('1')
         return () => (
             <div>
-                <want-button onClick={showBasic} block>Confirm</want-button>
-                <want-button onClick={showAlert} block>Alert</want-button>
-                <want-button onClick={showAfter} block>异步关闭</want-button>
+                <want-tabs v-model:value={activeName.value}>
+                    <want-tabs-pane name='1' label='标签一'>1</want-tabs-pane>
+                    <want-tabs-pane name='2' label='标签二'>2</want-tabs-pane>
+                    <want-tabs-pane name='3' label='标签三'>3</want-tabs-pane>
+                    <want-tabs-pane name='4' label='标签四'>4</want-tabs-pane>
+                    <want-tabs-pane name='5' label='标签五'>5</want-tabs-pane>
+                    <want-tabs-pane name='6' label='标签6'>6</want-tabs-pane>
+                </want-tabs>
             </div>
         )
     }
--- src/libs/Tabs/tabs.tsx
+++ src/libs/Tabs/tabs.tsx
@@ -7,6 +7,7 @@ export const Tabs = defineComponent({
     },
     setup(props, { slots, emit }) {
         const labelWrap = ref('')
+        const tabsWidth = labelWrap.value && labelWrap.value.getBoundingClientRect().width
         const activeStyle = computed(() => {
             const defaults: any = slots.default?.()
             const index = defaults.findIndex((s: VNode) => s.props?.name === props.value)
@@ -15,20 +16,36 @@ export const Tabs = defineComponent({
             const { left, width } = activeNode && activeNode.getBoundingClientRect()
             return { width: width + 'px', left: left + 'px' }
         })
+        const wrapStyle = computed(() => {
+            const defaults: any = slots.default?.()
+            const index = defaults.findIndex((s: VNode) => s.props?.name === props.value)
+            const nodes = labelWrap.value && labelWrap.value.querySelectorAll('.label')
+            let width = 0
+            let trans = 0
+            for (let i = 0; i < nodes.length; i++) {
+                width += nodes[i].parentElement.clientWidth
+            }
+            const activeLeft = Number(activeStyle.value.left?.replace('px', ''))||12
+            const nextLeft = activeLeft+nodes[index]?.parentElement.clientWidth
+            if(nextLeft>width){
+            }
+            console.log(nextLeft,width)
+            return { width: width + 'px' }
+        })
         const done = (prop: any) => {
             emit('update:value', prop.name)
-            emit('tab-click',prop)
+            emit('tab-click', prop)
         }
         const tabClick = (prop: any) => {
             if (prop.disabled) return
-            props.beforeClose ? props.beforeClose(()=>done(prop)) :done(prop)
+            props.beforeClose ? props.beforeClose(() => done(prop)) : done(prop)
         }
 
         provide('value', computed(() => props.value))
         return () => (
             <div class={t['want-tabs']}>
                 <div class={t['want-tabs-labels']}>
-                    <div class={t['want-tabs-labels-wrap']} ref={labelWrap}>
+                    <div class={t['want-tabs-labels-wrap']} ref={labelWrap} style={wrapStyle.value}>
                         {slots.default?.().map((l: any) => {
                             return <div class={t['label-wrap']} onClick={() => tabClick(l.props)} >
                                 <div class={[
