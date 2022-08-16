7074f08116d8ca4dbfc1640457d9ab0748a5001b
wantao <13813872403>
2022-08-16 17:09:08 +0800

    'picker组件pickerItem业务逻辑完成'

--- src/libs/Picker/index.tsx
+++ src/libs/Picker/index.tsx
@@ -4,42 +4,43 @@ import t from './index.module.scss'
 export const Picker = defineComponent({
     components: { PickerItem },
     props: {
+        title: String,
         visible: Boolean,
         columns: { type: Array, default: [] },
         value: { type: Array }
     },
-    emits: ['update:visible'],
+    emits: ['update:visible', 'update:value'],
     setup(props, { slots, emit }) {
         const visible = computed(() => props.visible)
-        const value = reactive([{}, {}, {}])
+        const value: any = reactive(['', '', ''])
         const yearIndex = ref(0)
         const monthIndex = ref(0)
-        const columns: any = reactive([]) 
-        for (let i = 2000; i < 2050; i++) {
-            columns.push({
-                label: i, children: Array.from(new Array(12).keys()).map(a => {
-                    return {
-                        label: a + 1, children: Array.from(new Array(new Date(i, a, 0).getDate()).keys()).map(d => {
-                            return { label: d + 1 }
-                        })
-                    }
-                })
-            })
-        }
+        const propsColums = props.columns
         const update = (value: boolean) => {
             emit('update:visible', value)
+        }
+        const cancel = () => {
+            emit('update:visible', false)
+        }
+        const confirm = () => {
+            emit('update:value', value)
+            emit('update:visible', false)
+        }
+        const updateValue = (value: any) => {
+            emit('update:value', value)
         }
         return () => (
             <want-popup visible={visible.value} onUpdate:visible={update} round>
                 <div class={t['want-picker']}>
                     <header>
-                        <want-button size='mini' fill='none' type='primary'>取消</want-button>
-                        <want-button size='mini' fill='none' type='primary'>确定</want-button>
+                        <want-button size='mini' fill='none' type='primary' onClick={cancel}>取消</want-button>
+                        <span>{slots.title?.() ?? props.title}</span>
+                        <want-button size='mini' fill='none' type='primary' onClick={confirm}>确定</want-button>
                     </header>
                     <main>
-                        <picker-item columns={columns} v-model:value={value[0]}></picker-item>
-                        <picker-item columns={columns[yearIndex.value].children} v-model:value={value[1]}></picker-item>
-                        <picker-item columns={columns[yearIndex.value].children[monthIndex.value].children} v-model:value={value[2]}></picker-item>
+                        <picker-item columns={propsColums} v-model:value={value[0]}></picker-item>
+                        {/* <picker-item columns={columns[yearIndex.value].children} v-model:value={value[1]}></picker-item>
+                        <picker-item columns={columns[yearIndex.value].children[monthIndex.value].children} v-model:value={value[2]}></picker-item> */}
                     </main>
                 </div>
             </want-popup>
